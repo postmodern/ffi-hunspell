b8ec839299d4f7f87d4a635fba9ebff76347c556
Postmodern <postmodern.mod3@gmail.com>
2010-10-06 16:37:20 -0700

    Added YARD documentation to Dictionary.

--- lib/ffi/hunspell/dictionary.rb
+++ lib/ffi/hunspell/dictionary.rb
@@ -40,10 +40,22 @@ module FFI
         end
       end
 
+      #
+      # The encoding of the dictionary file.
+      #
+      # @return [String]
+      #   The encoding of the dictionary file.
+      #
       def encoding
         Hunspell.Hunspell_get_dic_encoding(self)
       end
 
+      #
+      # Adds a word to the dictionary.
+      #
+      # @param [String] word
+      #   The word to add to the dictionary.
+      #
       def add(word)
         Hunspell.Hunspell_add(self,word.to_s)
       end
@@ -54,16 +66,40 @@ module FFI
 
       alias << add
 
+      #
+      # Removes a word from the dictionary.
+      #
+      # @param [String] word
+      #   The word to remove.
+      #
       def remove(word)
         Hunspell.Hunspell_remove(self,word.to_s)
       end
 
       alias delete remove
 
+      #
+      # Checks if the word is validate.
+      #
+      # @param [String] word
+      #   The word in question.
+      #
+      # @return [Boolean]
+      #   Specifies whether the word is valid.
+      #
       def check(word)
         Hunspell.Hunspell_spell(self,word.to_s) != 0
       end
 
+      #
+      # Finds the stems of a word.
+      #
+      # @param [String] word
+      #   The word in question.
+      #
+      # @return [Array<String>]
+      #   The stems of the word.
+      #
       def stem(word)
         stem_ptr = FFI::MemoryPointer.new(:pointer)
         count = Hunspell.Hunspell_stem(self,stem_ptr,word.to_s)
@@ -74,6 +110,15 @@ module FFI
         end
       end
 
+      #
+      # Suggests alternate spellings of a word.
+      #
+      # @param [String] word
+      #   The word in question.
+      #
+      # @return [Array<String>]
+      #   The suggestions for the word.
+      #
       def suggest(word)
         suggestion_ptr = FFI::MemoryPointer.new(:pointer)
         count = Hunspell.Hunspell_suggest(self,suggestion_ptr,word.to_s)
@@ -84,10 +129,19 @@ module FFI
         end
       end
 
+      #
+      # Closes the dictionary.
+      #
       def destroy
         Hunspell.Hunspell_destroy(self)
       end
 
+      #
+      # Converts the dictionary to a pointer.
+      #
+      # @return [FFI::Pointer]
+      #   The pointer for the dictionary.
+      #
       def to_ptr
         @ptr
       end
